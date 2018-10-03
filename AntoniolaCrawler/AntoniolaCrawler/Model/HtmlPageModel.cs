23a7eb25e85ac6fe9f3ab8d42d8bcc631849858a
Andrew Altunin <33705016+AltuninAndrew@users.noreply.github.com>
2018-10-03 23:22:23 +0300

    Add files via upload

---
+++ AntoniolaCrawler/AntoniolaCrawler/Model/HtmlPageModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntoniolaCrawler.Model
+{
+    class HtmlPageModel
+    {
+        private readonly WebClient _webClient;
+        private string _htmlPage;
+        private readonly string _htmlUri;
+
+        /*
+         * parser
+         * readonly Parser parser();
+         */
+
+        public HtmlPageModel(string uri)
+        {
+            _webClient = new WebClient();
+            _htmlUri = uri;
+        }
+
+        public void Initialize()
+        {
+            DownloadHtmlPage(_htmlUri);
+        }
+
+        private void DownloadHtmlPage(string url)
+        {
+            new Task(() =>
+            {
+                _htmlPage = _webClient.DownloadString(url);
+            }).Start();
+        }
+
+    }
+}
