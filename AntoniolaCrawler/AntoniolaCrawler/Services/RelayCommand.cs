b0e047d9d16bce8bf9b57512f69c0091d23f0fcf
rabinosona <rabinosona@gmail.com>
2018-10-04 20:17:12 +0300

    Added a few improvements to the UI, some business logic and boilerplate code were added

---
+++ AntoniolaCrawler/AntoniolaCrawler/Services/RelayCommand.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AntoniolaCrawler.Services
+{
+    // с просторов сети
+
+    public class RelayCommand : ICommand
+    {
+        #region Fields
+
+        readonly Action _execute;
+        readonly Func<bool> _canExecute;
+
+        #endregion // Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new command that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion // Constructors
+
+        #region ICommand Members
+
+        [DebuggerStepThrough]
+        public virtual bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public virtual void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        #endregion // ICommand Members
+    }
+
+}
--- AntoniolaCrawler/AntoniolaCrawler/ViewModel/MainWindowViewModel.cs
+++ AntoniolaCrawler/AntoniolaCrawler/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using AntoniolaCrawler.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace AntoniolaCrawler.ViewModel
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : PropertyChangedNotifier
     {
+        private int _searchDepth = 0;
+        private string _siteUri = "";
+
+        private ICommand _crawlSiteCommand;
+
+        // public ObservableCollection<Uri> SiteBypassLinks { get; set; }
+        // the task mentions that we should have a collection of links, but for now
+        // we can leave it to have only one link (solely for testing purpose)
+
+        public int SearchDepth
+        {
+            get => _searchDepth;
+            set
+            {
+                _searchDepth = value;
+                OnPropertyChanged(nameof(SearchDepth));
+            }
+        }
+
+        public string SiteUri
+        {
+            get => _siteUri;
+            set
+            {
+                _siteUri = value;
+                OnPropertyChanged(nameof(_siteUri));
+            }
+        }
+
+        public ICommand CrawlSiteCommand => _crawlSiteCommand ?? (_crawlSiteCommand = new RelayCommand(GetMetricsData));
+
+        private void GetMetricsData()
+        {
+            MessageBox.Show("Man, fuck this shit.");
+            SearchDepth = 1;
+        }
     }
 }
